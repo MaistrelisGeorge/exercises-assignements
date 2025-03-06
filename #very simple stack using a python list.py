a663be5a04350429d8c96d53f7366a223eed4951
MaistrelisGeorge <159917940+MaistrelisGeorge@users.noreply.github.com>
2025-03-06 15:25:32 +0200

    Add files via upload

---
+++ #very simple stack using a python list.py
@@ -0,0 +1,9 @@
+#very simple stack using a python list
+
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+print("The full stack is:", stack)  #expected output: [1, 2, 3] since it is using Lifo and append here adds at the start of the list
